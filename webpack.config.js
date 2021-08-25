7dc27de11f9a15389c7b052d90a4511f56843db8
Ilya Sukhoplyuev <suhoy@gramend.ru>
2021-08-25 14:36:58 +0400

    Карскас для React-приложения (webpack, Babel/JSX, Eslint, React)

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path")
+
+const webpack = require("webpack");
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+    entry: "./src/index.jsx",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "nutnet-weather.bundle.js",
+    },
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader",
+                options: {
+                    presets: [
+                        "@babel/preset-env",
+                        "@babel/react"
+                    ]
+                }
+            }
+        }]
+    },
+    devtool: false,
+    plugins: [
+        new webpack.SourceMapDevToolPlugin({}),
+        new ESLintPlugin({
+            context: ".",
+            extensions: ["js", "jsx"],
+            files: "src/"
+        })
+    ],
+};
